775246cdb65e1952f3e21593d096f74465bf5ae7
Raihaan Riswan <mohamedraihaanmohamedriswan@gmail.com>
2025-08-29 21:23:39 +0530

    update html

--- js/test.js
+++ js/test.js
@@ -93,3 +93,22 @@ function Load(){
 	window.location.href = "dashboard.html";
 
 }
+
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
